00bc8e114a26db4b29b5d99f4fa8008ac83aa545
Yulia <yuliya.f.s@gmail.com>
2023-09-26 13:08:04 +0300

    adding express handlebars crashing web server

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
-const PORT = 5000;
+const PORT = 3050;
 
+// //Handlebrs configuration
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', 'src/views');  //nastroiva se, zashtoto direktoriqta e razlichna ot defaultnata
+
+//Routes
 app.get('/', (req, res) => {
-    res.send('Hello...');
+    res.send('Hello..............!!!!!!!!')
+    res.render('index');
 });
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}...`));
