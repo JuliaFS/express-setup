4c1b4df9ca0aba23c81e94d815d0239611f6aeba
Yulia <yuliya.f.s@gmail.com>
2023-09-26 15:54:53 +0300

    added html in main.hbs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-//const path = require('path');
 const handlebars = require('express-handlebars');
 
 const app = express();
 
-const PORT = 3050;
+const PORT = 5000;
 
 // //Handlebrs configuration
 app.engine('hbs', handlebars.engine({
