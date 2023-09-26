2cab05673549fbabaf8d5071b1c4b053ab164b63
Yulia <yuliya.f.s@gmail.com>
2023-09-26 17:29:51 +0300

    added express config file to config directory

---
+++ src/config/expressConfig.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+
+function expressConfig(app){
+    //express config (inpolzvame middlewear za statichnite failove)
+    //app.use(express.static('src/public)'));                   //bi trqbvalo da raboti i taka, pri lektora se poluchi, pri men - ne
+    app.use(express.static(path.resolve(__dirname, '../public'))); //taka zadavame absoliuten pat
+}
+
+module.exports = expressConfig;
--- src/index.js
+++ src/index.js
@@ -2,20 +2,24 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 const path = require('path');
 
+const expressConfig = require('./config/expressConfig');
+
 const app = express();
 
 const PORT = 5000;
 
-//express config (inpolzvame middlewear za statichnite failove)
-app.use(express.static(path.resolve(__dirname, 'public')));
-
+// //express config (inpolzvame middlewear za statichnite failove)
+// //app.use(express.static('src/public)'));                   //bi trqbvalo da raboti i taka, pri lektora se poluchi, pri men - ne
+// app.use(express.static(path.resolve(__dirname, 'public'))); //taka zadavame absoliuten pat
+expressConfig(app);
 
 // //Handlebrs configuration
 app.engine('hbs', handlebars.engine({
     extname: 'hbs',
 }));
 app.set('view engine', 'hbs');
-app.set('views', 'src/views');  //nastroiva se, zashtoto direktoriqta e razlichna ot defaultnata
+//app.set('views', 'src/views');                              //nastroiva se, zashtoto direktoriqta e razlichna ot defaultnata
+app.set('views', path.resolve(__dirname, 'views'));
 
 //Routes
 app.get('/', (req, res) => {
