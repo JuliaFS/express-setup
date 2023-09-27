530a90ecb167debecf3f7942a0863545bb952963
Yulia <yuliya.f.s@gmail.com>
2023-09-27 09:23:05 +0300

    add cube details page

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -1,8 +1,25 @@
 const uniqid = require('uniqid');
 //const db = require('../db.json');
-const cubes = [];
+const cubes = [
+    {
+        id: 'sasdf234',
+        name: 'Mirror cube 1',
+        description: 'Mirror cube description',
+        imageUrl: 'https://images.pexels.com/photos/19677/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        difficultyLevel: 4
+    },
+    {
+        id: 'sasdf2344e',
+        name: 'Mirror cube 2',
+        description: 'Mirror cube 2 description',
+        imageUrl: 'https://images.pexels.com/photos/19677/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+        difficultyLevel: 4
+    }
+];
 
 exports.getAll = () => cubes.slice(); //vrashtame now masiv, a ne referenciq kam nego
+
+exports.getOne = (cubeId) => cubes.find(x => x.id == cubeId);
 
 //exports.create = (name, description, difficultyLevel, imageUrl);
 exports.create = (cubeData) => {
