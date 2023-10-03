781933862e052030fffefc416ca3dab0e6f35aff
Yulia <yuliya.f.s@gmail.com>
2023-10-03 11:39:55 +0300

    add accessory model

---
+++ src/models/Accessory.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const accessoryScema = new mongoose.Schema({
+    name: String,
+    description: String,
+    imageUrl: String
+});
+
+const Accessory = mongoose.model('Accessory', accessoryScema);
+
+module.exports = Accessory;
