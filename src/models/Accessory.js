4de9e4051286ccfeec7c28577e75973130e676ab
Yulia <yuliya.f.s@gmail.com>
2023-10-03 13:43:29 +0300

    change accessory.js in models

--- src/models/Accessory.js
+++ src/models/Accessory.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const accessoryScema = new mongoose.Schema({
     name: String,
     description: String,
-    imageUrl: String
+    imageUrl: String,
 });
 
 const Accessory = mongoose.model('Accessory', accessoryScema);
