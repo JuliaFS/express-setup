ae28d7550c48b92e7bf942aa8df85e9e7943b580
Yulia <yuliya.f.s@gmail.com>
2023-10-04 21:43:49 +0300

    add login snd register page as html

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const cubeController = require('./controllers/cubeController');
 const accessoryController = require('./controllers/accessoryController');
+const userController = require('./controllers/userController')
 
 router.use(homeController); //izpolzvame go kato middlewear
 router.use('/cubes', cubeController);
 router.use('/accessories', accessoryController);
+router.use('/users', userController);
 //router.use('/attach-accessory', cubeController);
 
 router.get('*', (req, res) => {
