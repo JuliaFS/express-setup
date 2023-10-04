0f975a532e869412f1fa4fa8cc374989f5b0ec85
Yulia <yuliya.f.s@gmail.com>
2023-10-04 22:34:46 +0300

    validate password/repeatPassword

---
+++ src/models/User.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    password: String,
+
+});
+
+userSchema.virtual('repeatPassword')
+    .set(function(value){
+        if(value !== this.password){
+            throw new mongoose.MongooseError('Password missmatch!');
+        }
+    });
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
