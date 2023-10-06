6ffe5bf0d80d330427315f9688b5b87cde80b430
Yulia <yuliya.f.s@gmail.com>
2023-10-06 21:54:36 +0300

    error handling added

--- src/utils/errorHelpers.js
+++ src/utils/errorHelpers.js
@@ -2,8 +2,10 @@ const { MongooseError } = require('mongoose');
 
 exports.extractErrorMessage = (error) => {
     if(error instanceof MongooseError){
+        console.log('Error is instance of Mongoose');
         return Object.values(error.errors).map( x => x.message);
     } else if(error){
+        console.log('Error is in error: ' + error.message);
         return [error.message];
     }
     // switch(typeof error){
